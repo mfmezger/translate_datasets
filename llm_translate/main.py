011847bc54e162b9a4823d70685724da940b91cf
Marc Mezger <marc.mezger@gmail.com>
2023-10-08 10:47:01 +0200

    adding datasets from huggingface.

--- llm_translate/main.py
+++ llm_translate/main.py
@@ -1,22 +1,34 @@
+import os
+import json
+from tqdm import tqdm
 from prefect import flow, task
-from typing import List
-import httpx
 
 
-@task(retries=3)
-def download_datasets(repo: str):
-    url = f"https://api.github.com/repos/{repo}"
 
-@task()
-def convert_to_csv(repo: str):
+def translate():
+    # call the ollama rest service with the instruction
     pass
 
 
-@flow(name="GitHub Stars")
-def dataset_preprocessing(repos: List[str]):
-    for repo in repos:
-        download_datasets(repo)
 
+
+# @task()
+def start_conversion(folder: str, file: str):+
+    tmp_dict = {}
+    # open the json file
+    with open(f"{folder}/{file}", "r") as json_file:
+        data = json.load(json_file)
+
+        for d in data:
+            pass
+
+
+
+
+# @flow(name="llm_translate")
+def dataset_preprocessing(folder):
+    for file in tqdm(os.listdir(folder)):
+        start_conversion(folder=folder, file=file)    
 
 # run the flow!
-dataset_preprocessing(["PrefectHQ/Prefect"])
+dataset_preprocessing(folder="data/input")
